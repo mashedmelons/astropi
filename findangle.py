dac644f9d853f689b4ee9c6da57115133cb1be87
Mash <88406995+mashedmelons@users.noreply.github.com>
2022-02-16 13:29:43 +0000

    woo

--- findangle.py
+++ findangle.py
@@ -1,12 +1,8 @@
 import numpy as np
 
-roll = np.pi/2
-pitch = 0
-yaw = 0
-
-phi = pitch+yaw
-theta = roll+yaw
-psi = roll+pitch
+phi = 0
+theta = np.pi
+psi = 0
 
 Rx = np.array([[1,0,0],[0,np.cos(phi),-np.sin(phi)],[0,np.sin(phi),np.cos(phi)]])
 Ry = np.array([[np.cos(theta),0,np.sin(theta)],[0,1,0],[-np.sin(theta),0,np.cos(theta)]])
@@ -26,6 +22,4 @@ for i in rotmateig[0]:
 rmeigvec = rotmateig[1]
 rotax = rmeigvec[:,2]
 
-print(rotmat)
 print(rotax)
-print(np.matmul(rotmat,rotax))
